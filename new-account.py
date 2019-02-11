4d6c4e71d345414a8c1437d7d391bef60095bcbd
Naimar1 <naiglyme1@gmail.com>
2019-02-11 17:39:59 +0200

    Put account save

--- new-account.py
+++ new-account.py
@@ -16,14 +16,35 @@ class CheckAccount(unittest.TestCase):
         '''
 
         self.new_account = Account("Ange","Ingabire","0788262733","ange@gmail.com","ange234")
-         def test_init(self):
-            self.assertEqual(self.new_account.first_name,"Ange")
-            self.assertEqual(self.new_account.last_name,"Ingabire")
-            self.assertEqual(self.new_account.phone_number,"0788262733")
-            self.assertEqual(self.new_account.email,"ange@gmail.com")
-            self.assertEqual(self.new_account.password,"ange234")
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
 
- if __name__ == '__main__':
+        self.assertEqual(self.new_account.first_name,"Ange")
+        self.assertEqual(self.new_account.last_name,"Ingabire")
+        self.assertEqual(self.new_account.phone_number,"0788262733")
+        self.assertEqual(self.new_account.email,"ange@gmail.com")
+        self.assertEqual(self.new_account.password,"ange234")
+
+    def test_save_account(self):
+        '''
+        test_save_account test case to test if the account object is saved into
+         the account list
+        '''
+        self.new_account.save_account() # saving the new account
+        self.assertEqual(len(account.account_list),1)
+        contact_list = [] 
+ 
+    def save_account(self):
+
+        '''
+        save_account method saves account objects into account_list
+        '''
+
+        Account.account_list.append(self)
+
+if __name__ == '__main__':
     unittest.main()
 
 
