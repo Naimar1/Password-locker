993013cee0348c982a9479c225234985b842d8c2
Naimar1 <naiglyme1@gmail.com>
2019-02-11 15:38:13 +0200

    Add docstring to clarify what i do

--- account.py
+++ account.py
@@ -2,3 +2,10 @@ import unittest
 from account import Account
 
 class CheckAccount(unittest.TestCase):
+    '''
+    Check class that define test cases for the account.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+
+    '''
