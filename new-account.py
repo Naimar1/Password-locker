0424c60db08ee9b1e3e16ea176964aa97f41a125
Naimar1 <naiglyme1@gmail.com>
2019-02-11 15:47:10 +0200

    Change file name

---
+++ new-account.py
@@ -0,0 +1,16 @@
+import unittest
+from account import Account
+
+class CheckAccount(unittest.TestCase):
+    '''
+    Check class that define test cases for the account class.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
