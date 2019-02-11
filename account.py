88fb89dcba6f23811d24a948de7d116b91477170
Naimar1 <naiglyme1@gmail.com>
2019-02-11 15:57:42 +0200

    Add parameters

--- account.py
+++ account.py
@@ -3,4 +3,16 @@ class Account:
     Class that show new instances of account
     '''
 
-    
+    def __init__(self,first_name,last_name,phone_number,email,password):
+
+         '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New account first name.
+            last_name : New account last name.
+            number: New account phone number.
+            email : New account email address.
+            password : New account password.
+            
+        '''
