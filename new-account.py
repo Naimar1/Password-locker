51a1bd529a78973bca9fd2981d506d96e5a6fdc8
Naimar1 <naiglyme1@gmail.com>
2019-02-11 16:27:42 +0200

    Add method that check expected result

--- new-account.py
+++ new-account.py
@@ -14,3 +14,13 @@ class CheckAccount(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
+
+        self.new_account = Account("Ange","Ingabire","0788262733","ange@gmail.com","ange234")
+         def test_init(self):
+            self.assertEqual(self.new_account.first_name,"Ange")
+            self.assertEqual(self.new_account.last_name,"Ingabire")
+            self.assertEqual(self.new_account.phone_number,"0788262733")
+            self.assertEqual(self.new_account.email,"ange@gmail.com")
+            self.assertEqual(self.new_account.password,"ange234")
+
+
